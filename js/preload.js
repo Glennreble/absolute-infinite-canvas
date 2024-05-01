cd45c908a42f4da97decbf81adb670769809480a
rodan12 <141339442+Glennreble@users.noreply.github.com>
2024-05-01 18:56:26 +0800

    Update preload.js

--- js/preload.js
+++ js/preload.js
@@ -5,5 +5,5 @@ function loadImage(src)
     return img;
 }
 
-var shapeImages = [loadImage("images/imgtool/grass.png"), loadImage("images/imgtool/sand.png"), loadImage("images/imgtool/ice.png"),
+var shapeImages = [loadImage("images/imgtool/grass.png"), loadImage("images/imgtool/sand.png"), loadImage("images/imgtool/ice.png"), loadImage("images/imgtool/Difficultyface_cooldudecrafter_Pack.png",
     loadImage("images/imgtool/earth.png"),loadImage("images/imgtool/saturn.png"), loadImage("images/imgtool/cook1eegames.png"), loadImage("images/imgtool/mandelbrot.png")];
