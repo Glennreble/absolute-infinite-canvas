a12e23921b5ea71f7bb1482cb7ee03a7f3fd5b26
rodan12 <141339442+Glennreble@users.noreply.github.com>
2024-05-01 19:46:45 +0800

    Update preload.js

--- js/preload.js
+++ js/preload.js
@@ -6,4 +6,5 @@ function loadImage(src)
 }
 
 var shapeImages = [loadImage("images/imgtool/grass.png"), loadImage("images/imgtool/sand.png"), loadImage("images/imgtool/ice.png"),
-    loadImage("images/imgtool/earth.png"),loadImage("images/imgtool/saturn.png"), loadImage("images/imgtool/cook1eegames.png"), loadImage("images/imgtool/mandelbrot.png"), loadImage("images/imgtool/Difficultyface_cooldudecrafter_Pack.png")];
+    loadImage("images/imgtool/earth.png"),loadImage("images/imgtool/saturn.png"), loadImage("images/imgtool/cook1eegames.png"), loadImage("images/imgtool/mandelbrot.png"), loadImage("images/imgtool/Difficultyface_cooldudecrafter_Pack.png"),
+    loadImage("images/imgtool/unrated (1).png"), loadImage("images/imgtool/easy (1).png")];
