66bb2f9de26bdb2ce7942f6ca733cdeb2b03e89e
rodan12 <141339442+Glennreble@users.noreply.github.com>
2024-05-03 19:44:38 +0800

    Update preload.js

--- js/preload.js
+++ js/preload.js
@@ -7,4 +7,5 @@ function loadImage(src)
 
 var shapeImages = [loadImage("images/imgtool/grass.png"), loadImage("images/imgtool/sand.png"), loadImage("images/imgtool/ice.png"),
     loadImage("images/imgtool/earth.png"),loadImage("images/imgtool/saturn.png"), loadImage("images/imgtool/cook1eegames.png"), loadImage("images/imgtool/mandelbrot.png"), loadImage("images/imgtool/Difficultyface_cooldudecrafter_Pack.png"),
-    loadImage("images/imgtool/unrated (1).png"), loadImage("images/imgtool/easy (1).png")];
+    loadImage("images/imgtool/unrated (1).png"), loadImage("images/imgtool/easy (1).png"), loadImage("images/imgtool/hard.png"), loadImage("images/imgtool/harder.png"), loadImage("images/imgtool/insane.png"),
+    loadImage("images/imgtool/demon-easy.png")];
